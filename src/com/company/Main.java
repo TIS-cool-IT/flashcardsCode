463839d06e022dc00ac6975e4c5904af097b81b3
Mato <martin.fiala21@gmail.com>
2015-11-05 18:16:33 +0100

    prvy commit od mata

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
-z
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hey COOOL IT !");
         // Slavka je tu
+        //maťo
     }
 }
