b140b77ba4aff5929097e4dbbb09ccd7d91594ef
Slavka Ivanicova <slavka5212@gmail.com>
2015-11-05 18:05:03 +0100

    Slavka prvy commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,10 @@
 package com.company;
-
+z
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hey COOOL IT !");
+        // Slavka je tu
     }
 }
