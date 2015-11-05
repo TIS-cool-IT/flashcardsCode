aacc37b1db37af7d4185d025f5d07f1faf3efedf
stefi92 <stefanec.michal@zsstvorlistok.sk>
2015-11-05 18:30:26 +0100

    creating project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hey COOOL IT !");
+        System.out.println("hey COOOL IT ! riadna kokotina podme uz");
     }
 }
